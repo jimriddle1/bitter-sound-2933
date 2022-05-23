bc5a7d60f0ccdec9f001f5f4674ad35a3dcedea0
Jim Riddle <jimriddle0@gmail.com>
2022-05-23 15:42:07 -0500

    added create to the project contestants controller to pass extension_2, but after time expired

--- app/controllers/project_contestants_controller.rb
+++ app/controllers/project_contestants_controller.rb
@@ -1,7 +1,7 @@
 class ProjectContestantsController < ApplicationController
 
   def create
-    binding.pry
-    # redirect_to "projects"
+    @contestant_project = ContestantProject.create!(contestant_id: params[:id], project_id: params[:project_id])
+    redirect_to "/projects/#{params[:project_id]}"
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,20 +1,11 @@
 class ProjectsController < ApplicationController
 
-  # def index
-  #   @projects = Project.all
-  # end
+
 
   def show
     @project = Project.find(params[:id])
   end
-  #
-  def create
-    project = Project.last
-    @contestant = Contestant.find(params[:id])
-    @contestant_project_1 = ContestantProject.create!(contestant_id: @contestant.id, project_id: project.id)
-    # binding.pry
-    redirect_to "/projects/#{project.id}"
-  end
+  
 
   private
 
